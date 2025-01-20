197f241d50e4dedfe7919bf673ebf54a8b3d8125
prempkSF <premkumar.prithivirajan@syncfusion.com>
2025-01-20 17:55:41 +0530

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "account",
+    pathMatch : "full"
+  },
+  {
+    path : "account",
+    loadChildren : ()=> import('./account/account.module').then((module)=>module.AccountModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
